d3348fd77b33def771be248c6ca107ef90830cbc
Nahuel Ripoll <nahuelripoll33@gmail.com>
2022-09-05 13:56:12 -0300

    Curso React

---
+++ contador-de-clics/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import freeCodeCampLogo from './images/FreeCodeCamp_logo.png';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="freecodecamp-logo-contenedor">
+        <img 
+          className='freecodecamp-logo' 
+          src={freeCodeCampLogo}
+          alt='Logo de freecodecamp' />
+      </div>
+      <div className='contenedor-principal'>
+          
+      </div>
+    </div>
+  );
+}
+
+
+export default App;
